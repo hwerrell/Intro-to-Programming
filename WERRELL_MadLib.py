b9cc23a65158e841fbd5a437127f21ad4fce0e9d
hwerrell <hewe9760@colorado.edu>
2025-10-15 13:52:15 -0600

    Add files via upload

---
+++ WERRELL_MadLib.py
@@ -0,0 +1,39 @@
+## Variables
+name1 = input("Enter a name: ")
+verb1 = input("Enter a verb (past tense): ")
+adjective1 = input("Enter an adjective: ")
+adjective2 = input("Enter an adjective: ")
+adverb1 = input("Enter an adverb: ")
+number1 = input("Enter a number: ")
+adjective3 = input("Enter an adjective: ")
+bodyPart1 = input("Enter a body part: ")
+interjection1 = input("Enter an interjection: ")
+adjective4 = input("Enter an adjective: ")
+monsterWord1 = input("Enter a monster word: ")
+adjective5 = input("Enter an adjective: ")
+typeOfBug1 = input("Enter a type of bug: ")
+verb2 = input("Enter a verb (past tense): ")
+bodyPart2 = input("Enter a body part: ")
+greeting1 = input("Enter a greeting: ")
+adjective6 = input("Enter an adjective: ")
+noun1 = input("Enter a noun: ")
+name2 = input("Enter a name: ")
+adjective7 = input("Enter an adjective: ")
+number2 = input("Enter a number: ")
+monsterPart1 = input("Enter a monster part: ")
+emotion1 = input("Enter an emotion: ")
+adjective8 = input("Enter an adjective: ")
+
+
+## Printed story
+story = (f"""{name1} the monster was late for his first day of school! He {verb1} out of his {adjective1} bed, and rubbed the sleep out of his {adjective2} eyes. 
+As {adverb1} as he could, he pulled socks onto all {number1} of his feet. He ran downstairs and shoveled some {adjective3} cereal into his {bodyPart1}. 
+His mom yelled, {interjection1}! You're going to miss the {adjective4} bus! 
+
+He grabbed his {monsterWord1}ology textbook, threw some {adjective5} {typeOfBug1}s into his lunchbox, and {verb2} out the door. 
+His neighbor waved his {bodyPart2} at him, and said {greeting1}! {name1} tried to wave back but he tripped over his {adjective6} {noun1}. 
+He stood up, just in time to see {name2}, the bus driver, open the {adjective7} doors to the bus. 
+
+{name1} squeezed all {number2} of his {monsterPart1}s into a seat and breathed a sigh of {emotion1}. Finally, he was on his way to another {adjective8} day of monster school.""")
+
+print(story)
